b93637430240b5b7a6dae7c3dc0e61c8ff9514da
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2015-02-15 22:05:49 -0600

    Enhancement: Removed profiles.clj

--- profiles.clj
+++
@@ -1,2 +0,0 @@
-{:dev-env-vars  {:env {:database-url "postgres://auth_user:password1@127.0.0.1:5432/auth_local?stringtype=unspecified"}}
- :test-env-vars {:env {:database-url "postgres://auth_user:password1@127.0.0.1:5432/auth_test?stringtype=unspecified"}}}
