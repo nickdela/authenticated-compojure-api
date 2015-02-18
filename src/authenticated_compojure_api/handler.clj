7e52e43d15464dc62c3d73dbb00d85439ddab450
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2015-02-17 21:44:46 -0600

    Enhancement: Removed all quote routes. We are user api only now

--- src/authenticated_compojure_api/handler.clj
+++ src/authenticated_compojure_api/handler.clj
@@ -1,7 +1,6 @@
 (ns authenticated-compojure-api.handler
   (:require [compojure.api.sweet :refer :all]
             [authenticated-compojure-api.routes.user :refer :all]
-            [authenticated-compojure-api.routes.quotes :refer :all]
             [authenticated-compojure-api.middleware.basic-auth :refer [basic-auth-mw]]
             [authenticated-compojure-api.middleware.token-auth :refer [token-auth-mw]]
             [authenticated-compojure-api.middleware.cors :refer [cors-mw]]
@@ -11,7 +10,6 @@
 
 (defn init []
   (query/create-registered-user-table-if-not-exists!)
-  (query/create-quotes-table-if-not-exists!)
   (query/create-permission-table-if-not-exists!)
   (query/create-user-permission-table-if-not-exists!)
   (query/create-password-reset-key-table-if-not-exists!))
@@ -24,8 +22,4 @@
 
   (swaggered "User"
              :description "Create and reset user details"
-             user-routes)
-
-  (swaggered "Quote"
-             :description "Create Read Update and Delete quotes"
-             quote-routes))
+             user-routes))
