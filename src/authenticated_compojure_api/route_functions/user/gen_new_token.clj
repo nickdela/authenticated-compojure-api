167d8e61f37544f652efe9f36b806a3ce08d8bb3
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2015-02-19 21:47:20 -0600

    Style: variable name

--- src/authenticated_compojure_api/route_functions/user/gen_new_token.clj
+++ src/authenticated_compojure_api/route_functions/user/gen_new_token.clj
@@ -4,8 +4,8 @@
             [buddy.sign.generic :as bs]
             [ring.util.http-response :as respond]))
 
-(defn gen-new-token-response [refresh_token]
-  (let [user (first (query/get-registered-user-by-reset-token {:refresh_token refresh_token}))]
+(defn gen-new-token-response [refresh-token]
+  (let [user (first (query/get-registered-user-by-reset-token {:refresh_token refresh-token}))]
     (if (empty? user)
       (respond/bad-request {:error "Bad Request"})
       (respond/ok          {:token (create-token user)}))))
