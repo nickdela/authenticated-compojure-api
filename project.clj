1115312f127f4ef248b99784a568def04163b199
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2015-05-28 08:29:33 -0500

    Dependency: Update dependencies to current versions

--- project.clj
+++ project.clj
@@ -2,13 +2,13 @@
   :description "An example compojure-api app with authentication using buddy"
 
   :dependencies [[org.clojure/clojure        "1.6.0"]
-                 [metosin/compojure-api      "0.20.0"]
-                 [metosin/ring-http-response "0.6.1"]
+                 [metosin/compojure-api      "0.21.0"]
+                 [metosin/ring-http-response "0.6.2"]
                  [metosin/ring-swagger-ui    "2.1.1-M2"]
-                 [cheshire                   "5.4.0"]
-                 [http-kit                   "2.1.18"]
-                 [buddy                      "0.5.2"]
-                 [org.clojure/java.jdbc      "0.3.6"]
+                 [cheshire                   "5.5.0"]
+                 [http-kit                   "2.1.19"]
+                 [buddy                      "0.5.4"]
+                 [org.clojure/java.jdbc      "0.3.7"]
                  [postgresql/postgresql      "9.3-1102.jdbc41"]
                  [yesql                      "0.5.0-rc2"]
                  [environ                    "1.0.0"]
@@ -25,8 +25,7 @@
                        :aot :all}
 
              :test-local   {:dependencies [[javax.servlet/servlet-api "2.5"]
-                                           [ring-mock                 "0.1.5"]
-                                           [cheshire                  "5.4.0"]]}
+                                           [ring-mock                 "0.1.5"]]}
 
              ;; Set these in ./profiles.clj
              :test-env-vars {}
