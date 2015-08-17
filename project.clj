a0522dc39462dd94d4e2cf0a6d90f9a21b75769a
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2015-08-16 21:06:31 -0500

    Dependency: Bump dependency versions to current

--- project.clj
+++ project.clj
@@ -1,18 +1,18 @@
 (defproject authenticated-compojure-api "0.1.0-SNAPSHOT"
   :description "An example compojure-api app with authentication using buddy"
 
-  :dependencies [[org.clojure/clojure        "1.6.0"]
-                 [metosin/compojure-api      "0.21.0"]
-                 [metosin/ring-http-response "0.6.2"]
-                 [metosin/ring-swagger-ui    "2.1.1-M2"]
+  :dependencies [[org.clojure/clojure        "1.7.0"]
+                 [metosin/compojure-api      "0.22.2"]
+                 [metosin/ring-http-response "0.6.3"]
+                 [metosin/ring-swagger-ui    "2.1.1"]
                  [cheshire                   "5.5.0"]
                  [http-kit                   "2.1.19"]
-                 [buddy                      "0.5.4"]
-                 [org.clojure/java.jdbc      "0.3.7"]
+                 [buddy                      "0.6.1"]
+                 [org.clojure/java.jdbc      "0.4.1"]
                  [postgresql/postgresql      "9.3-1102.jdbc41"]
-                 [yesql                      "0.5.0-rc2"]
+                 [yesql                      "0.5.0"]
                  [environ                    "1.0.0"]
-                 [clj-time                   "0.9.0"]
+                 [clj-time                   "0.11.0"]
                  [com.draines/postal         "1.11.3"]]
 
   :plugins      [[lein-environ "1.0.0"]]
