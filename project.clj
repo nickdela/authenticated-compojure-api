88a7221eb9f81fab5a0e094aad6db3b114396351
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2018-05-16 10:27:59 -0500

    Version 0.3.1 -> 0.3.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject authenticated-compojure-api/lein-template "0.3.1"
+(defproject authenticated-compojure-api/lein-template "0.3.2"
   :description "compojure-api with token-based authentication using Buddy."
   :url "https://github.com/JarrodCTaylor/authenticated-compojure-api"
   :license {:name "MIT"
